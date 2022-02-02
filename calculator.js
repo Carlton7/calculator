fda5d87213134593d1756cdb230080070d12e44f
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-02 15:42:05 +0000

    basic req complete for calculator challenge

--- calculator.js
+++ calculator.js
@@ -1,2 +1,38 @@
 `use strict`
+
 import * as DOM from './dom.js';
+
+function add(n1,n2) {
+  let total = 0;
+  total = parseInt(n1, 10) + parseInt(n2, 10);
+  result(total);
+}
+
+function subtract(n1,n2) {
+  let total = 0;
+  total = parseInt(n1, 10) - parseInt(n2, 10);
+  result(total);
+}
+
+function multiply(n1,n2) {
+  let total = 0;
+  total = parseInt(n1, 10) * parseInt(n2, 10);
+  result(total);
+}
+
+function divide(n1,n2) {
+  let total = 0;
+  total = parseInt(n1, 10) / parseInt(n2, 10);
+  result(total);
+}
+
+function result(total) {
+  let list = document.createElement(`li`);
+  list.innerHTML = total;
+  DOM.listOutput.appendChild(list);
+}
+
+DOM.addBtn.onclick = () => add(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.subtractBtn.onclick = () => subtract(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.multiplyBtn.onclick = () => multiply(DOM.inputOne.value, DOM.inputTwo.value);
+DOM.divideBtn.onclick = () => divide(DOM.inputOne.value, DOM.inputTwo.value);
--- dom.js
+++ dom.js
@@ -1,1 +1,7 @@
 export const inputOne = document.getElementById(`input_1`);
+export const inputTwo = document.getElementById(`input_2`);
+export const listOutput = document.getElementById(`list_output`);
+export const addBtn = document.getElementById(`button_add`);
+export const subtractBtn = document.getElementById(`button_subtract`);
+export const multiplyBtn = document.getElementById(`button_multiply`);
+export const divideBtn = document.getElementById(`button_divide`);
