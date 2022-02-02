847252fa4400f3890b0cf42305b12330562383a2
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-02 15:55:06 +0000

    changing regular functions to arrow functions

--- calculator.js
+++ calculator.js
@@ -2,31 +2,34 @@
 
 import * as DOM from './dom.js';
 
-function add(n1,n2) {
-  let total = 0;
-  total = parseInt(n1, 10) + parseInt(n2, 10);
+//OLD FORMAT, BELOW ARE CHANGED TO ARROW FUNCTIONS
+// function add(n1,n2) {
+//   let total = 0;
+//   total = parseInt(n1, 10) + parseInt(n2, 10);
+//   result(total);
+// }
+
+let add = (n1,n2) => {
+  let total = parseInt(n1, 10) + parseInt(n2, 10);
   result(total);
 }
 
-function subtract(n1,n2) {
-  let total = 0;
-  total = parseInt(n1, 10) - parseInt(n2, 10);
+let subtract = (n1,n2) => {
+  let total = parseInt(n1, 10) - parseInt(n2, 10);
   result(total);
 }
 
-function multiply(n1,n2) {
-  let total = 0;
-  total = parseInt(n1, 10) * parseInt(n2, 10);
+let multiply = (n1,n2) => {
+  let total = parseInt(n1, 10) * parseInt(n2, 10);
   result(total);
 }
 
-function divide(n1,n2) {
-  let total = 0;
-  total = parseInt(n1, 10) / parseInt(n2, 10);
+let divide = (n1,n2) => {
+  let total = parseInt(n1, 10) / parseInt(n2, 10);
   result(total);
 }
 
-function result(total) {
+let result = (total) => {
   let list = document.createElement(`li`);
   list.innerHTML = total;
   DOM.listOutput.appendChild(list);
