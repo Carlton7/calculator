75075e802e170a07e72d277d40610e0e4b3d315f
CARLTON ANAND <carlton.anand@gmail.com>
2022-02-02 16:46:57 +0000

    cleaning equals and removing result

--- calculator.js
+++ calculator.js
@@ -36,14 +36,16 @@ let divide = (n1,n2) => {
 }
 
 let equals = (storage) => {
-  result(storage)
-}
-
-let result = (storage) => {
   let list = document.createElement(`li`);
   list.innerHTML = storage;
   DOM.listOutput.appendChild(list);
 }
+
+// let result = (storage) => {
+//   let list = document.createElement(`li`);
+//   list.innerHTML = storage;
+//   DOM.listOutput.appendChild(list);
+// }
 
 DOM.addBtn.onclick = () => add(DOM.inputOne.value, DOM.inputTwo.value);
 DOM.subtractBtn.onclick = () => subtract(DOM.inputOne.value, DOM.inputTwo.value);
